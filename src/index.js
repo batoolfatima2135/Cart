c2939d4b5c0419dd36fb7f0ea52cd97e8e2225c6
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-14 19:41:49 +0500

    Modified main pages to add components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './App/Store';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+   
   </React.StrictMode>
 );
